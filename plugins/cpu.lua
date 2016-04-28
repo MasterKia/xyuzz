d9337e310a8ddf326c4de2ea9e8d2c0e3707bb30
MrTorento <seyedalikia27@gmail.com>
2016-04-28 14:55:08 +0430

    Update cpu.lua

--- plugins/cpu.lua
+++ plugins/cpu.lua
@@ -1,7 +1,16 @@
 function run_sh(msg)
      name = get_name(msg)
      text = ''
-     
+     -- if config.sh_enabled == false then 
+        -- text = '!sh command is disabled'
+     -- else
+        -- if is_sudo(msg) then
+           -- bash = msg.text:sub(4,-1)
+           -- text = run_bash(bash)
+        -- else
+           -- text = name .. ' you have no power here!'
+        -- end
+     -- end
 	 if is_sudo(msg) then
         bash = msg.text:sub(4,-1)
         text = run_bash(bash)
@@ -39,7 +48,7 @@ end
 
 function run(msg, matches)
   if not is_sudo(msg) then
-    return ""
+    return "You aren't allowed!"
   end
   local receiver = get_receiver(msg)
   if string.match(msg.text, '!sh') then
